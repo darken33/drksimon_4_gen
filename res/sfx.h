c506e3c04453d768968f0d0f2a868479eeff01c3
Philippe Bousquet <pbousquet@sqli.com>
2017-11-16 18:51:04 +0100

    resources generation

---
+++ res/sfx.h
@@ -0,0 +1,9 @@
+#ifndef _RES_SFX_H_
+#define _RES_SFX_H_
+
+extern const u8 snd1_sfx[10496];
+extern const u8 snd2_sfx[10496];
+extern const u8 snd3_sfx[10496];
+extern const u8 snd4_sfx[10752];
+
+#endif // _RES_SFX_H_
---
+++ res/sprite.h
@@ -0,0 +1,16 @@
+#ifndef _RES_SPRITE_H_
+#define _RES_SPRITE_H_
+
+extern const SpriteDefinition simon_off_spr;
+extern const SpriteDefinition blue_on_spr;
+extern const SpriteDefinition green_on_spr;
+extern const SpriteDefinition red_on_spr;
+extern const SpriteDefinition yellow_on_spr;
+extern const SpriteDefinition bad_spr;
+extern const SpriteDefinition good_spr;
+extern const SpriteDefinition play_spr;
+extern const SpriteDefinition title_txt;
+extern const SpriteDefinition start_txt;
+extern const SpriteDefinition difficulty_txt;
+
+#endif // _RES_SPRITE_H_
